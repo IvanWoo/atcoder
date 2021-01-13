b2628f4c589022e80ed3e7d9e2783b45845b3588
Yifan Wu <tyivanwu@gmail.com>
2021-01-12 23:32:56 -0500

    chore: add abc080/A B

---
+++ contests/abc080/A/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define forn(i, x, y) for(int i = x; i < y; i++)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define all(s) s.begin(), s.end()
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) (a + b - 1) / b
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+void solve(long long N, long long A, long long B)
+{
+    int answer = min(N * A, B);
+    cout << answer << endl;
+}
+
+int main()
+{
+    long long N;
+    scanf("%lld", &N);
+    long long A;
+    scanf("%lld", &A);
+    long long B;
+    scanf("%lld", &B);
+    solve(N, A, B);
+    return 0;
+}
---
+++ contests/abc080/B/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define forn(i, x, y) for(int i = x; i < y; i++)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define all(s) s.begin(), s.end()
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) (a + b - 1) / b
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(long long N)
+{
+    string s = to_string(N);
+    int sum = 0;
+    for (char c : s)
+    {
+        sum += c - '0';
+    }
+    cout << (N % sum == 0 ? YES : NO) << endl;
+}
+
+int main()
+{
+    long long N;
+    scanf("%lld", &N);
+    solve(N);
+    return 0;
+}
