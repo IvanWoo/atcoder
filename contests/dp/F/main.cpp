dcc34c27d1b60133d415d62f5caca6fa2bdc349c
Yifan Wu <tyivanwu@gmail.com>
2021-10-29 18:53:15 -0400

    chore: add dp/F

---
+++ contests/dp/F/main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define forn(i, x, y) for(int i = x; i < y; i++)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define all(s) s.begin(), s.end()
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) (a + b - 1) / b
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+// viz: https://ja.wikipedia.org/wiki/最長共通部分列問題
+void solve(std::string s, std::string t)
+{
+    int m = s.size(), n = t.size();
+    VVLL dp(m + 1, VLL(n + 1, 0));
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (s[i] == t[j])
+                chmax(dp[i + 1][j + 1], dp[i][j] + 1);
+            chmax(dp[i + 1][j + 1], dp[i + 1][j]);
+            chmax(dp[i + 1][j + 1], dp[i][j + 1]);
+        }
+    }
+
+    string res = "";
+    int i = m, j = n;
+    while (i > 0 && j > 0)
+    {
+        if (dp[i][j] == dp[i - 1][j])
+        {
+            i--;
+        }
+        else if (dp[i][j] == dp[i][j - 1])
+        {
+            j--;
+        }
+        else
+        {
+            res = s[i - 1] + res;
+            i--;
+            j--;
+        }
+    }
+
+    cout << res << endl;
+}
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    std::string t;
+    std::cin >> t;
+    solve(s, t);
+    return 0;
+}
