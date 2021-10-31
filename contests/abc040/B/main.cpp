fb1891625787453a501379c7d981e0fb8b474ada
Yifan Wu <tyivanwu@gmail.com>
2021-10-31 11:53:13 -0400

    chore: add abc040/B

---
+++ contests/abc040/B/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define forn(i, x, y) for(int i = x; i < y; i++)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define all(s) s.begin(), s.end()
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) for(auto const &it: a) cout << it << ' ';
+#define DEBUGVV(a) for(auto const &it: a) { DEBUGV(it); cout << endl; };
+#define CEIL(a, b) (a + b - 1) / b
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+const int INF = 1LL << 30;
+void solve(long long n)
+{
+    int res = INF;
+    LL m = sqrt(n);
+    for (int i = 1; i <= m; i++)
+    {
+        int q = n / i;
+        int r = n % i;
+        chmin(res, abs(q - i) + r);
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    long long n;
+    std::scanf("%lld", &n);
+    solve(n);
+    return 0;
+}
